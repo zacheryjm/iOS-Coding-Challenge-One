083340209e107d98ee157a77a8faf19810860fe6
zmiller <zmiller@cs.odu.edu>
2019-01-28 10:42:15 -0500

    Initial Commit

---
+++ iOS Coding Challenge One/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS Coding Challenge One
+//
+//  Created by Zachery Miller on 1/28/19.
+//  Copyright © 2019 Zachery Miller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
